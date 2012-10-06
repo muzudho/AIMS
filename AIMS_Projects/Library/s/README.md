fbec96d9894142b43090a75e15f0e04880f6309e
鬼畜大王 <kichikudaiou@gmail.com>
2012-10-07 06:38:29 +0900

    Power up.

--- AIMS_Projects/Library/s/README.md
+++ AIMS_Projects/Library/s/README.md
@@ -1,6 +1,9 @@
 ﻿*はじめに(Introduction)
 
-シーン移動を自動化します。
+ライセンスについては、一番上の階層の README.md を見て下さい。
+
+
+*シーン移動を自動化します。
 
 	使い方。
 
@@ -16,24 +19,25 @@
 		（例）
 		NO,SCENE,Expl,BTN1,BTN2,BTN3,BTN4,BTN5,BTN6,BTN7,BTN8,BTN9,BTN10,BTN11,BTN12,END
 		int,int,string,int,int,int,int,int,int,int,int,int,int,int,int,END
-		-1,シーン名。「scene」の後ろに連結される文字列。空白または「-1」を入れると無視されます。,解説,ボタン1を押したときの移動先シーン名。「scene」の後ろに連結される文字列。空白または「-1」を入れると無視されます。,ボタン2,ボタン3,ボタン4,ボタン5,ボタン6,ボタン7,ボタン8,ボタン9,ボタン10,ボタン11,ボタン12,END
-		0,1,,-1,2,3,4,5,6,7,8,9,10,11,12,END
-		1,2,,1,-1,3,4,5,6,7,8,9,10,11,12,END
-		2,3,,1,2,-1,4,5,6,7,8,9,10,11,12,END
-		3,4,,1,2,3,-1,5,6,7,8,9,10,11,12,END
-		4,5,,1,2,3,4,-1,6,7,8,9,10,11,12,END
-		5,6,,1,2,3,4,5,-1,7,8,9,10,11,12,END
-		6,7,,1,2,3,4,5,6,-1,8,9,10,11,12,END
-		7,8,,1,2,3,4,5,6,7,-1,9,10,11,12,END
-		8,9,,1,2,3,4,5,6,7,8,-1,10,11,12,END
-		9,10,,1,2,3,4,5,6,7,8,9,-1,11,12,END
-		10,11,,1,2,3,4,5,6,7,8,9,10,-1,12,END
-		11,12,,1,2,3,4,5,6,7,8,9,10,11,-1,END
+		-1,シーン名。空白または「-1」を入れると無視されます。,解説,ボタン1を押したときの移動先シーン名。空白または「-1」を入れると無視されます。,ボタン2,ボタン3,ボタン4,ボタン5,ボタン6,ボタン7,ボタン8,ボタン9,ボタン10,ボタン11,ボタン12,END
+		-1,,"Sorry, Encoding of this csv file is Shift-JIS.（このCSVファイルはShift-JISエンコーディングです。）",,,,,,,,,,,,,END
+		0,scene1,,,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		1,scene2,,scene1,,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		2,scene3,,scene1,scene2,,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		3,scene4,,scene1,scene2,scene3,,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		4,scene5,,scene1,scene2,scene3,scene4,,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		5,scene6,,scene1,scene2,scene3,scene4,scene5,,scene7,scene8,scene9,scene10,scene11,scene12,END
+		6,scene7,,scene1,scene2,scene3,scene4,scene5,scene6,,scene8,scene9,scene10,scene11,scene12,END
+		7,scene8,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,,scene9,scene10,scene11,scene12,END
+		8,scene9,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,,scene10,scene11,scene12,END
+		9,scene10,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,,scene11,scene12,END
+		10,scene11,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,,scene12,END
+		11,scene12,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,,END
 		EOF,,,,,,,,,,,,,,,
 
 	（３）シーンの _OnStep の最後で、次の関数を呼び出します。
 
-		auto_ForwardScene(1);	--引数は自シーン番号。
+		auto_ForwardScene("scene1");	--引数は自シーン名。
 
 	これで、押したボタンに応じて changeScene します。
 	以上。
--- AIMS_Projects/Library/s/auto.lua
+++ AIMS_Projects/Library/s/auto.lua
@@ -20,35 +20,34 @@
 		（例）
 		NO,SCENE,Expl,BTN1,BTN2,BTN3,BTN4,BTN5,BTN6,BTN7,BTN8,BTN9,BTN10,BTN11,BTN12,END
 		int,int,string,int,int,int,int,int,int,int,int,int,int,int,int,END
-		-1,シーン名。「scene」の後ろに連結される文字列。空白または「-1」を入れると無視されます。,解説,ボタン1を押したときの移動先シーン名。「scene」の後ろに連結される文字列。空白または「-1」を入れると無視されます。,ボタン2,ボタン3,ボタン4,ボタン5,ボタン6,ボタン7,ボタン8,ボタン9,ボタン10,ボタン11,ボタン12,END
-		-1,-1,"Sorry, Encoding of this file is Shift-JIS.（このファイルはShift-JISエンコーディングです。）",,,,,,,,,,,,,END
-		0,1,,-1,2,3,4,5,6,7,8,9,10,11,12,END
-		1,2,,1,-1,3,4,5,6,7,8,9,10,11,12,END
-		2,3,,1,2,-1,4,5,6,7,8,9,10,11,12,END
-		3,4,,1,2,3,-1,5,6,7,8,9,10,11,12,END
-		4,5,,1,2,3,4,-1,6,7,8,9,10,11,12,END
-		5,6,,1,2,3,4,5,-1,7,8,9,10,11,12,END
-		6,7,,1,2,3,4,5,6,-1,8,9,10,11,12,END
-		7,8,,1,2,3,4,5,6,7,-1,9,10,11,12,END
-		8,9,,1,2,3,4,5,6,7,8,-1,10,11,12,END
-		9,10,,1,2,3,4,5,6,7,8,9,-1,11,12,END
-		10,11,,1,2,3,4,5,6,7,8,9,10,-1,12,END
-		11,12,,1,2,3,4,5,6,7,8,9,10,11,-1,END
+		-1,シーン名。空白または「-1」を入れると無視されます。,解説,ボタン1を押したときの移動先シーン名。空白または「-1」を入れると無視されます。,ボタン2,ボタン3,ボタン4,ボタン5,ボタン6,ボタン7,ボタン8,ボタン9,ボタン10,ボタン11,ボタン12,END
+		-1,,"Sorry, Encoding of this csv file is Shift-JIS.（このCSVファイルはShift-JISエンコーディングです。）",,,,,,,,,,,,,END
+		0,scene1,,,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		1,scene2,,scene1,,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		2,scene3,,scene1,scene2,,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		3,scene4,,scene1,scene2,scene3,,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		4,scene5,,scene1,scene2,scene3,scene4,,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
+		5,scene6,,scene1,scene2,scene3,scene4,scene5,,scene7,scene8,scene9,scene10,scene11,scene12,END
+		6,scene7,,scene1,scene2,scene3,scene4,scene5,scene6,,scene8,scene9,scene10,scene11,scene12,END
+		7,scene8,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,,scene9,scene10,scene11,scene12,END
+		8,scene9,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,,scene10,scene11,scene12,END
+		9,scene10,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,,scene11,scene12,END
+		10,scene11,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,,scene12,END
+		11,scene12,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,,END
 		EOF,,,,,,,,,,,,,,,
 
 	（３）シーンの _OnStep の最後で、次の関数を呼び出します。
 
-		auto_ForwardScene(1);	--引数は自シーン番号。
+		auto_ForwardScene("scene1");	--引数は自シーン名。
 
 	これで、押したボタンに応じて changeScene します。
-	（シーン名は数字とします）
 	以上。
 ]]
 
 
 --外部ファイルと照らし合わせて、シーン移動します。
---currentScenePartsName:自シーン名の「scene」の後ろに連結されている文字列。空白または「-1」は入れないでください。
-function auto_ForwardScene(currentScenePartsName)
+--current_NameScene:自シーン名。空白または「-1」は入れないでください。
+function auto_ForwardScene(current_NameScene)
 
 
 	--データベース・テーブル
@@ -59,7 +58,7 @@ function auto_ForwardScene(currentScenePartsName)
 	local rowNum = 1
 	for number_Row,record in pairs(t) do
 		if 4<=rowNum then
-			if tonumber(record["SCENE"])==currentScenePartsName then
+			if record["SCENE"]==current_NameScene then
 				hitsRecord = record
 				break
 			end
@@ -69,65 +68,65 @@ function auto_ForwardScene(currentScenePartsName)
 	end
 
 	-- ゲームパッドの、十字キー以外のボタンを受け付けます。
-	local nextScenePartsName = ""
+	local next_NameScene = ""
 	if(isJoyPressed(BUTTON_TRIG1))then
 		-- ゲームパッドのボタン1が今押されている
-		nextScenePartsName = hitsRecord["BTN1"];
+		next_NameScene = hitsRecord["BTN1"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG2))then
 		-- ゲームパッドのボタン2が今押されている
-		nextScenePartsName = hitsRecord["BTN2"];
+		next_NameScene = hitsRecord["BTN2"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG3))then
 		-- ゲームパッドのボタン3が今押されている
-		nextScenePartsName = hitsRecord["BTN3"];
+		next_NameScene = hitsRecord["BTN3"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG4))then
 		-- ゲームパッドのボタン4が今押されている
-		nextScenePartsName = hitsRecord["BTN4"];
+		next_NameScene = hitsRecord["BTN4"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG5))then
 		-- ゲームパッドのボタン5が今押されている
-		nextScenePartsName = hitsRecord["BTN5"];
+		next_NameScene = hitsRecord["BTN5"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG6))then
 		-- ゲームパッドのボタン6が今押されている
-		nextScenePartsName = hitsRecord["BTN6"];
+		next_NameScene = hitsRecord["BTN6"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG7))then
 		-- ゲームパッドのボタン7が今押されている
-		nextScenePartsName = hitsRecord["BTN7"];
+		next_NameScene = hitsRecord["BTN7"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG8))then
 		-- ゲームパッドのボタン8が今押されている
-		nextScenePartsName = hitsRecord["BTN8"];
+		next_NameScene = hitsRecord["BTN8"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG9))then
 		-- ゲームパッドのボタン9が今押されている
-		nextScenePartsName = hitsRecord["BTN9"];
+		next_NameScene = hitsRecord["BTN9"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG10))then
 		-- ゲームパッドのボタン10が今押されている
-		nextScenePartsName = hitsRecord["BTN10"];
+		next_NameScene = hitsRecord["BTN10"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG11))then
 		-- ゲームパッドのボタン11が今押されている
-		nextScenePartsName = hitsRecord["BTN11"];
+		next_NameScene = hitsRecord["BTN11"];
 	end
 
 	if(isJoyPressed(BUTTON_TRIG12))then
 		-- ゲームパッドのボタン12が今押されている
-		nextScenePartsName = hitsRecord["BTN12"];
+		next_NameScene = hitsRecord["BTN12"];
 	end
 
 	--ゲームパッドのボタンの定数は BUTTON_TRIG1～BUTTON_TRIG12 （１２個）までしか存在しない。
@@ -136,9 +135,9 @@ function auto_ForwardScene(currentScenePartsName)
 	--CSVから読み取った値は、数字であっても、文字列型になっています。
 
 	--シーン番号は、空白、または「-1」を無視します。
-	if( "" ~= nextScenePartsName and "-1" ~= nextScenePartsName )then
+	if( "" ~= next_NameScene and "-1" ~= next_NameScene )then
 		--文字列連結してシーン名を作成します。
-		changeScene("scene" .. nextScenePartsName) -- シーン切替。
+		changeScene(next_NameScene) -- シーン切替。
 	end
 
 end
