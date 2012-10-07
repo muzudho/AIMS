d574a0c86c0e0fd47bda950d0e49d25d6cfd970e
鬼畜大王 <kichikudaiou@gmail.com>
2012-10-07 10:07:40 +0900

    Power up, too.

--- AIMS_Projects/Library/s/auto.lua
+++ AIMS_Projects/Library/s/auto.lua
@@ -18,27 +18,20 @@
 		data/sceneChanges.csv
 
 		（例）
-		NO,SCENE,Expl,BTN1,BTN2,BTN3,BTN4,BTN5,BTN6,BTN7,BTN8,BTN9,BTN10,BTN11,BTN12,END
-		int,int,string,int,int,int,int,int,int,int,int,int,int,int,int,END
-		-1,シーン名。空白または「-1」を入れると無視されます。,解説,ボタン1を押したときの移動先シーン名。空白または「-1」を入れると無視されます。,ボタン2,ボタン3,ボタン4,ボタン5,ボタン6,ボタン7,ボタン8,ボタン9,ボタン10,ボタン11,ボタン12,END
-		-1,,"Sorry, Encoding of this csv file is Shift-JIS.（このCSVファイルはShift-JISエンコーディングです。）",,,,,,,,,,,,,END
-		0,scene1,,,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
-		1,scene2,,scene1,,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
-		2,scene3,,scene1,scene2,,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
-		3,scene4,,scene1,scene2,scene3,,scene5,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
-		4,scene5,,scene1,scene2,scene3,scene4,,scene6,scene7,scene8,scene9,scene10,scene11,scene12,END
-		5,scene6,,scene1,scene2,scene3,scene4,scene5,,scene7,scene8,scene9,scene10,scene11,scene12,END
-		6,scene7,,scene1,scene2,scene3,scene4,scene5,scene6,,scene8,scene9,scene10,scene11,scene12,END
-		7,scene8,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,,scene9,scene10,scene11,scene12,END
-		8,scene9,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,,scene10,scene11,scene12,END
-		9,scene10,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,,scene11,scene12,END
-		10,scene11,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,,scene12,END
-		11,scene12,,scene1,scene2,scene3,scene4,scene5,scene6,scene7,scene8,scene9,scene10,scene11,,END
-		EOF,,,,,,,,,,,,,,,
+		NO,ID,Expl,ALWAYS,DISABLED_AT_LOADING,THREAD_NOT_RUNNING,BTN1,BTN1_PARAM,BTN2,BTN2_PARAM,BTN3,BTN3_PARAM,BTN4,BTN4_PARAM,BTN5,BTN5_PARAM,BTN6,BTN6_PARAM,BTN7,BTN7_PARAM,BTN8,BTN8_PARAM,BTN9,BTN_PARAM,BTN10,BTN10_PARAM,BTN11,BTN11_PARAM,BTN12,BTN12_PARAM,END
+		int,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,END
+		-1,auto_ForwardScene(...)が置かれている関数名を入れると便利です。識別できれば何でも構わない。空白または「-1」を入れると無視されます。,解説,この列に設定されていると、必ずこれが選ばれます。,「Yes」…isLoaderRunning=true （データロード中）の場合、ボタン入力によるシーン遷移を無効にします。「No、-1、(空白)」…なにもしません。,isThreadRunning()==false（スレッドが終了している）の場合、ボタンよりも優先的にこれが選ばれます。（空白、または-1以外が設定されている場合）,ボタン1を押したときの移動先シーン名。空白または「-1」を入れると無視されます。,「Push」…一度ボタンを離してから押さないと反応しません。「(空白)または-1」…押していれば反応します。,ボタン2,ボタン2,ボタン3,ボタン3,ボタン4,ボタン4,ボタン5,ボタン5,ボタン6,ボタン6,ボタン7,ボタン7,ボタン8,ボタン8,ボタン9,ボタン9,ボタン10,ボタン10,ボタン11,ボタン11,ボタン12,ボタン12,END
+		-1,,"Sorry, Encoding of this csv file is Shift-JIS.（このCSVファイルはShift-JISエンコーディングです。）",,,,,,,,,,,,,,,,,,,,,,,,,,,,END
+		0,boot_OnStart,,logo,,,,,,,,,,,,,,,,,,,,,,,,,,,END
+		1,logo_OnStep(DONT_USE),CSVファイルが読込み完了していないことがあるので、このレコードは使えない。,,Yes,title,title,,,,,,,,,,,,,,,,,,,,,,,,END
+		2,title_OnStep,,,,,game,Push,,,,,,,,,,,,,,,,,,,,,,,END
+		3,thread_gameover,,gameover,,,,,,,,,,,,,,,,,,,,,,,,,,,END
+		4,gameover_OnStep,,,,,title,,,,,,,,,,,,,,,,,,,,,,,,END
+		EOF,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 
-	（３）シーンの _OnStep の最後で、次の関数を呼び出します。
+	（３）シーンを切り替えたいタイミングで、次の関数を呼び出します。
 
-		auto_ForwardScene("scene1");	--引数は自シーン名。
+		auto_ForwardScene("scene1_OnStep");	--引数は、これを呼び出している関数の名前を入れると便利。
 
 	これで、押したボタンに応じて changeScene します。
 	以上。
@@ -46,8 +39,8 @@
 
 
 --外部ファイルと照らし合わせて、シーン移動します。
---current_NameScene:自シーン名。空白または「-1」は入れないでください。
-function auto_ForwardScene(current_NameScene)
+--id:この関数を置いている関数の名前を入れると便利です。空白または「-1」は入れないでください。
+function auto_ForwardScene(id)
 
 
 	--データベース・テーブル
@@ -58,7 +51,7 @@ function auto_ForwardScene(current_NameScene)
 	local rowNum = 1
 	for number_Row,record in pairs(t) do
 		if 4<=rowNum then
-			if record["SCENE"]==current_NameScene then
+			if record["ID"]==id then
 				hitsRecord = record
 				break
 			end
@@ -68,65 +61,68 @@ function auto_ForwardScene(current_NameScene)
 	end
 
 	-- ゲームパッドの、十字キー以外のボタンを受け付けます。
-	local next_NameScene = ""
-	if(isJoyPressed(BUTTON_TRIG1))then
-		-- ゲームパッドのボタン1が今押されている
-		next_NameScene = hitsRecord["BTN1"];
+	local next_NameScene
+	next_NameScene = hitsRecord["ALWAYS"];
+	if("-1"==next_NameScene)then
+		--空白および「-1」は空白扱い（入力を無かったことに）。
+		next_NameScene = ""
 	end
 
-	if(isJoyPressed(BUTTON_TRIG2))then
-		-- ゲームパッドのボタン2が今押されている
-		next_NameScene = hitsRecord["BTN2"];
+	if(""==next_NameScene and isLoaderRunning())then
+		--データロード中のとき。
+		next_NameScene = hitsRecord["DISABLED_AT_LOADING"];
+		if("Yes"==next_NameScene)then
+			--ボタン押下による画面遷移を無視します。
+			next_NameScene = "-1"
+		end
 	end
 
-	if(isJoyPressed(BUTTON_TRIG3))then
-		-- ゲームパッドのボタン3が今押されている
-		next_NameScene = hitsRecord["BTN3"];
+	if(""==next_NameScene and isThreadRunning()==false)then
+		--スレッドが走っていないとき。
+		next_NameScene = hitsRecord["THREAD_NOT_RUNNING"];
+		if("-1"==next_NameScene)then
+			--空白および「-1」は空白扱い（入力を無かったことに）。
+			next_NameScene = ""
+		end
 	end
 
-	if(isJoyPressed(BUTTON_TRIG4))then
-		-- ゲームパッドのボタン4が今押されている
-		next_NameScene = hitsRecord["BTN4"];
-	end
+	local button_trig = {
+		BUTTON_TRIG1,
+		BUTTON_TRIG2,
+		BUTTON_TRIG3,
+		BUTTON_TRIG4,
+		BUTTON_TRIG5,
+		BUTTON_TRIG6,
+		BUTTON_TRIG7,
+		BUTTON_TRIG8,
+		BUTTON_TRIG9,
+		BUTTON_TRIG10,
+		BUTTON_TRIG11,
+		BUTTON_TRIG12
+	}
+	local btnParam, hits
+	local btnNum=1
+	--ボタン1～12。
+	while(""==next_NameScene and btnNum<=12)do
 
-	if(isJoyPressed(BUTTON_TRIG5))then
-		-- ゲームパッドのボタン5が今押されている
-		next_NameScene = hitsRecord["BTN5"];
-	end
+		-- BTN1_PARAM ～ BTN12_PARAM
+		btnParam = hitsRecord[ "BTN" .. btnNum .. "_PARAM" ];
+		if( btnParam=="Push" )then
+			if( 1==getJoyPressCount(0,button_trig[btnNum]) )then
+				-- ゲームパッドのボタンNが、いったん離された後に押された時。
+				hits = true
+			end
+		elseif( 0<getJoyPressCount(0,button_trig[btnNum]) )then
+			-- ゲームパッドのボタンNは、今押されている。
+			hits = true
+		end
+		if( hits )then
+			-- BTN1 ～ BTN12
+			next_NameScene = hitsRecord[ "BTN" .. btnNum ];
+			break;
+		end
 
-	if(isJoyPressed(BUTTON_TRIG6))then
-		-- ゲームパッドのボタン6が今押されている
-		next_NameScene = hitsRecord["BTN6"];
-	end
-
-	if(isJoyPressed(BUTTON_TRIG7))then
-		-- ゲームパッドのボタン7が今押されている
-		next_NameScene = hitsRecord["BTN7"];
-	end
-
-	if(isJoyPressed(BUTTON_TRIG8))then
-		-- ゲームパッドのボタン8が今押されている
-		next_NameScene = hitsRecord["BTN8"];
-	end
-
-	if(isJoyPressed(BUTTON_TRIG9))then
-		-- ゲームパッドのボタン9が今押されている
-		next_NameScene = hitsRecord["BTN9"];
-	end
-
-	if(isJoyPressed(BUTTON_TRIG10))then
-		-- ゲームパッドのボタン10が今押されている
-		next_NameScene = hitsRecord["BTN10"];
-	end
-
-	if(isJoyPressed(BUTTON_TRIG11))then
-		-- ゲームパッドのボタン11が今押されている
-		next_NameScene = hitsRecord["BTN11"];
-	end
-
-	if(isJoyPressed(BUTTON_TRIG12))then
-		-- ゲームパッドのボタン12が今押されている
-		next_NameScene = hitsRecord["BTN12"];
+		btnNum = btnNum + 1
 	end
 
 	--ゲームパッドのボタンの定数は BUTTON_TRIG1～BUTTON_TRIG12 （１２個）までしか存在しない。
